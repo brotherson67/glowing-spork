8c50a949adc6172aeae947bb18292a5ca6916435
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2022-02-09 06:26:32 -0700

    moved app to home page and refactored app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import React, { useState } from 'react';
-import Sidebar from './components/Sidebar';
-import Nav from './components/Nav';
-import Ski from './components/Ski';
-import Bike from './components/Bike';
-import Run from './components/Run';
-import Footer from './components/footer';
-// import MountainBike from './components/mountain-bike';
-// import Hero from './components/Hero';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
-  const [isOpen, setIsOpen] = useState(false)
-  const toggle = () => {
-    //set state to go from false to true
-    setIsOpen(!isOpen)
-    console.log(isOpen);
-
-  }
-
   return (
     <div>
       <main>
-        <Router>
-          <Nav toggle={toggle} />
-          {isOpen ? < Sidebar toggle={toggle} /> : ''}
-          
-          <Routes>
-            {/* <Route path="/" element={<Home/>} /> */}
-            <Route path="/ski" element={<Ski />} />
-            <Route path="/mountain-bike" element={<Bike />} />
-            <Route path="/trail-run" element={<Run />} />
-          </Routes>
+        <Router> 
+          <Home />
         </Router>
-        {/* <Footer></Footer>  */}
-        
-       
-      
-      <Footer /> 
-
       </main>
     </div>
   );

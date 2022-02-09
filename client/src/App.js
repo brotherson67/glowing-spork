9ed17608691d5d8b46b2007ef14290952e9dc458
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2022-02-09 13:57:54 -0700

    added social feed page

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import React, { useState } from 'react';
-import Sidebar from './components/Sidebar';
-import Nav from './components/Nav';
-import Ski from './components/Ski';
-import Bike from './components/Bike';
-import Run from './components/Run';
-import Footer from './components/footer';
-import SocialFeed from './components/SocialFeed';
-// import Hero from './components/Hero';
-// import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Home from './pages/Home';
 
 function App() {
@@ -16,17 +6,8 @@ function App() {
     <div>
       <main>
         <Router>
-          {/* <Nav toggle={toggle} /> */}
-          {/* {isOpen ? < Sidebar toggle={toggle} /> : ''} */}
+          <Home />
           
-          <Routes>
-            
-            <Route path="/ski" element={<Ski />} />
-            <Route path="/mountain-bike" element={<Bike />} />
-            <Route path="/trail-run" element={<Run />} />
-            <Route path="/social" element={<SocialFeed />} />
-            <Route path="/" element={<Home />} />
-          </Routes>
           
         </Router>
       </main>

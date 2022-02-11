4338c3d3ca082ae7d9792126c1fa03c028f4c32f
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2022-02-11 01:11:21 -0700

    added nav bar activities dropdown menu. Routes still work and it is still responsive

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,36 +1,52 @@
 import React from 'react';
 import { Nav, Bars, NavMenu, NavLink, NavBtn } from './NavbarEl';
+import { Dropdown } from 'react-bootstrap';
 
-const  Navbar = ({ toggle }) => {
+const Navbar = ({ toggle }) => {
 
-  return (
-  <>
-  <Nav>
-      <NavLink to="/">
-          <h1>R-o-u-t-e-s</h1>
-      </NavLink>
-      <Bars onClick={toggle} />
-      <NavMenu>
-        <NavLink to="/ski">
-            Ski
-        </NavLink>
-        <NavLink to="/mountain-bike">
-            Mountain bike
-        </NavLink>
-        <NavLink to="/trail-run">
-            Trail Run
-        </NavLink>
-        <NavLink to="/social">
-            Social Feed
-        </NavLink>
-      </NavMenu>
-      {/* <NavBtn>
+    return (
+        <>
+            <Nav>
+                <NavLink to="/">
+                    <h1>R-o-u-t-e-s</h1>
+                </NavLink>
+                <Bars onClick={toggle} />
+                <NavMenu>
+                    <NavLink to="/social">
+                        Social Feed
+                    </NavLink>
+                    <Dropdown>
+                        <Dropdown.Toggle >Activities</Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            <Dropdown.Item>
+                                <NavLink to="/ski">
+                                    Ski
+                                </NavLink>
+                            </Dropdown.Item>
+                            <Dropdown.Item>
+                                <NavLink to="/mountain-bike">
+                                    Mountain bike
+                                </NavLink>
+                            </Dropdown.Item>
+                            <Dropdown.Item>
+                                <NavLink to="/trail-run">
+                                    Trail Run
+                                </NavLink>
+                            </Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+
+
+
+
+                </NavMenu>
+                {/* <NavBtn>
           <Button to='/login' primary='true'>
               Login
           </Button>
       </NavBtn> */}
-  </Nav>
-  </>
+            </Nav>
+        </>
     );
 }
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import React, { useState } from 'react';
 import Sidebar from '../components/Sidebar';
 import Nav from '../components/Nav';
@@ -21,6 +22,7 @@ function Home() {
           <Nav toggle={toggle} />
           {isOpen ? < Sidebar toggle={toggle} /> : ''}
           <Routes>
+          
             <Route path="/" element={<Hero/>} />
             <Route path="/ski" element={<Ski />} />
             <Route path="/mountain-bike" element={<Bike />} />

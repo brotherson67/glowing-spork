f65adf19bf0d3a77986c2757b84f776615f9224e
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2022-02-14 15:41:39 -0700

    thoughtlist is rendered to social feed page

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,21 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import Home from './pages/Home';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+
+
+//eastablish link to graphql backend
+const httpLink = createHttpLink({
+  uri: 'http://127.0.0.1:3001/graphql',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
+    <ApolloProvider client={client}>
     <div>
       <main>
         <Router>
@@ -10,6 +23,7 @@ function App() {
         </Router>
       </main>
     </div>
+    </ApolloProvider>
   );
 }
 
--- client/src/pages/ContentFeed.js
+++ client/src/pages/ContentFeed.js
@@ -1,15 +1,28 @@
 import React from 'react';
-// import coverImage from "../../assets/images/IMG_1939.JPEG";
-// import Hero from '../Hero';
+import { useQuery } from '@apollo/client';
+import { QUERY_THOUGHTS } from '../utils/queries';
+import PostList from '../components/Posts';
 
-function ContentFeed() {
+
+const ContentFeed = () => {
+    //useQuery to make a query request
+    const { loading, data } = useQuery(QUERY_THOUGHTS);
+    //use optional chaining to negate checking if object exists before accessing its properties
+    const thoughts = data?.thoughts || [];
+    console.log(thoughts);
+
     return (
         <div>
             <section>
             {/* <Hero img={coverImage}/> */}
-            Text
+            <div>
+            {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <PostList thoughts={thoughts} title="Find your next adventure!" />
+      )}
+            </div>
             </section>
-            
         </div>
     );
 }

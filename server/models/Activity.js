8ae55b16403920c40f7fc8fe3cf7568920a3c129
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2022-02-14 18:13:48 -0700

    re adding thoughts model

--- server/models/Activity.js
+++ server/models/Activity.js
@@ -1,19 +1,19 @@
-const { Schema, model } = require("mongoose");
+// const { Schema, model } = require("mongoose");
 
-const activitySchema = new Schema({
-  activity: {
-    type: String,
-  },
-  preferedDifficulty: {
-    type: String,
-  },
-  yearsExperience: {
-    type: Number,
-  },
-});
+// const activitySchema = new Schema({
+//   activity: {
+//     type: String,
+//   },
+//   preferedDifficulty: {
+//     type: String,
+//   },
+//   yearsExperience: {
+//     type: Number,
+//   },
+// });
 
-const Activity = model("Activity", activitySchema);
+// const Activity = model("Activity", activitySchema);
 
-module.exports = Activity;
+// module.exports = Activity;
 
 // Idea for later: create gear owned entity

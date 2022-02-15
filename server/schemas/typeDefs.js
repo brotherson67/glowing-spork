830d18b495b2c5af39be0d0db5d2de5f1e396d44
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2022-02-14 18:41:31 -0700

    seedes changed

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -5,7 +5,6 @@ const { gql } = require("apollo-server-express");
 //type queries are methods you come up with that will be used to read you database
 //mutations are used to alter data base, similar to update delete
 const typeDefs = gql`
-<<<<<<< HEAD
     type Thought{
         _id: ID
         thoughtText: String
@@ -52,46 +51,6 @@ const typeDefs = gql`
       addReaction(thoughtId: ID!, reactionBody: String!): Thought
       addFriends(friendId: ID!): User
     }
-=======
-  type Post {
-    _id: ID
-    postText: String
-    createdAt: String
-    username: String
-  }
-
-  type User {
-    _id: ID
-    username: String
-    posts: [Post]
-    friends: [User]
-    friendCount: Int
-    activities: [Activity]
-    reactions: [Reaction]
-  }
-
-  type Activity {
-    _id: ID
-    activity: String
-    participants: [User]
-  }
-
-  type Reaction {
-    _id: ID
-    reactionBody: String
-  }
-
-  type Query {
-    users(username: String): [User]
-    posts(username: String): [Post]
-  }
-
-  type Mutation {
-    createUser(username: String!, email: String!, password: String!): User
-    createPost(postText: String!, username: String!): Post
-    login(username: String!, password: String!): User
-  }
->>>>>>> 6954ce375b6d38e184a90ff60078df46b21d263e
 `;
 
 // export the typeDefs

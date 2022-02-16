fc9a32d2ce561253a6329d47eba64e41fd2bdd53
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2022-02-16 01:09:28 -0700

    connection file changed

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.connect(
 
 
-  process.env.MONGODB_URI || 'mongodb://localhost/routes',
+  process.env.MONGODB_URI || 'mongodb://localhost/Routes',
 
 
   {

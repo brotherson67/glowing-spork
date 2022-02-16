14b380258b5d2b5919c633f878eeabd04fc836df
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2022-02-16 01:06:14 -0700

    heroku test deploy 9

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.connect(
 
 
-  process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017/routes',
+  process.env.MONGODB_URI || 'mongodb://localhost/routes',
 
 
   {

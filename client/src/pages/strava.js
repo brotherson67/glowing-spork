b7eb992774854546b3691862b6c8cf85f3f4c043
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2022-02-15 20:02:09 -0700

    strava stuff not working

--- client/src/pages/strava.js
+++ client/src/pages/strava.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useNavigate, useLocation, useParams } from "react-router-dom";
+// import axios from "axios";
 // import './app.css';
 import "../utils/strava";
 // import polyline from '@mapbox/polyline'
@@ -12,79 +13,6 @@ function StravaActivities() {
   const params = useParams();
   console.log(window.location.search)
   const client_secret = 'ba4cf64706994d406df016b09df6d62ee55edaef';
-
-  useEffect(() => {
-    console.log(params)
-    if (code) {
-      function convertAuthToAccess() {
-        const authLink = `https://www.strava.com/oauth/token?client_id=${client_id}&client_secret=${client_secret}&code=${code}&grant_type3=authorization_code
-        `
-        console.log(authLink)
-      }
-      convertAuthToAccess();
-      // add other function call to get user id
-
-      function getActivities(res) {
-        // commented out path to get user profile info from an array
-        // https://www.strava.com/api/v3/athlete?access_token=7bc114015e81808697585c211d65f269c319a3cc
-        const activitiesLink = `https://www.strava.com/api/v3/athlete/activities?access_token=${res.access_token}`
-
-        fetch(activitiesLink)
-          //   .then((res) => console.log(res.json()))
-          .then((res) => {
-            return res.json();
-          })
-          .then(data => {
-            console.log(data);
-            // var htmlContent = "";
-            // for (let i = 0; i > 5; i++) {
-            // document.getElementById('activity').innerHTML = data[0];
-            document.getElementById('activity-name').textContent = 'Activity Name: ' + data[0].name;
-
-            document.getElementById('activity-distance').innerHTML = 'Distance: ' + data[0].distance;
-
-            document.getElementById('averageSpeed').innerHTML = 'Average Speed: ' + data[0].average_speed;
-
-            document.getElementById('totalTime').innerHTML = 'Total Time: ' + data[0].elapsed_time;
-
-            document.getElementById('elevationGain').innerHTML = 'Total Elevation Gain: ' + data[0].total_elevation_gain;
-
-          })
-      }
-
-      //   getActivities();
-
-      //refresh token
-      //https://www.strava.com/oauth/token?client_id=77814&client_secret=ba4cf64706994d406df016b09df6d62ee55edaef&refresh_token=e3c3bce7513bb09b9c19bfd2450855830fb0d313&grant_type=refresh_token
-
-      const authLink = "https://www.strava.com/oauth/token";
-
-      function reAuthorize() {
-        fetch(authLink, {
-          method: 'post',
-          headers: {
-            'Accept': 'application/json, text/plain, */*',
-            'Content-Type': 'application/json'
-
-          },
-          body: JSON.stringify({
-            client_id: '77814',
-            client_secret: 'ba4cf64706994d406df016b09df6d62ee55edaef',
-            refresh_token: 'e3c3bce7513bb09b9c19bfd2450855830fb0d313',
-            grant_type: 'refresh_token'
-          })
-          //get response json it and pass it to activities
-        }).then((res => res.json()))
-          .then(res => getActivities(res))
-
-      }
-
-      reAuthorize();
-    }
-
-  },
-    [])
-
   const url = new URLSearchParams(window.location.search)
   // const term = url.get('term')
   console.log(url)
@@ -98,16 +26,102 @@ function StravaActivities() {
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, ' '));
   }
-
-
   var code = getParameterByName('code');
   console.log(code)
-
 
   function handleClick() {
     window.location.href = `https://www.strava.com/oauth/authorize?client_id=${client_id}&redirect_uri=http://localhost:3000/profile&response_type=code&scope=activity:read_all
         `
   }
+  const authLink = "https://www.strava.com/oauth/token?";
+  
+  // if (code) {
+  function getAccessToken(data) {
+    fetch(authLink, {
+      method: 'post',
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+
+      },
+      body: JSON.stringify({
+        client_id: '77814',
+        client_secret: client_secret,
+        code: 'code',
+        grant_type: 'authorization_code'
+      })
+      //get response json it and pass it to activities
+    })
+    .then((res) => console.log(res.json()))
+    // .then(res => handleClick)
+
+  
+console.log(getAccessToken)
+  getAccessToken();
+  // add other function call to get user id
+  // convertAuthToAccess();
+  function getActivities(res) {
+  //   // commented out path to get user profile info from an array
+  //   // https://www.strava.com/api/v3/athlete?access_token=7bc114015e81808697585c211d65f269c319a3cc
+    const activitiesLink = `https://www.strava.com/api/v3/athlete/activities?access_token=${res.access_token}`
+
+    fetch(activitiesLink)
+      // .then((res) => console.log(res.json()))
+      .then((res) => {
+        return res.json();
+      })
+      .then(data => {
+        console.log(data);
+        // var htmlContent = "";
+        // for (let i = 0; i > 5; i++) {
+        // document.getElementById('activity').innerHTML = data[0];
+        document.getElementById('activity-name').textContent = 'Activity Name: ' + data[0].name;
+
+        document.getElementById('activity-distance').innerHTML = 'Distance: ' + data[0].distance;
+
+        document.getElementById('averageSpeed').innerHTML = 'Average Speed: ' + data[0].average_speed;
+
+        document.getElementById('totalTime').innerHTML = 'Total Time: ' + data[0].elapsed_time;
+
+        document.getElementById('elevationGain').innerHTML = 'Total Elevation Gain: ' + data[0].total_elevation_gain;
+
+      })
+  }
+
+    getActivities();
+
+  //refresh token
+  //https://www.strava.com/oauth/token?client_id=77814&client_secret=ba4cf64706994d406df016b09df6d62ee55edaef&refresh_token=e3c3bce7513bb09b9c19bfd2450855830fb0d313&grant_type=refresh_token
+
+  // const authLink = "https://www.strava.com/oauth/token";
+
+  function reAuthorize() {
+    fetch(authLink, {
+      method: 'post',
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+
+      },
+      body: JSON.stringify({
+        client_id: '77814',
+        client_secret: 'ba4cf64706994d406df016b09df6d62ee55edaef',
+        refresh_token: 'e3c3bce7513bb09b9c19bfd2450855830fb0d313',
+        grant_type: 'refresh_token'
+      })
+      //get response json it and pass it to activities
+    }).then((res => res.json()))
+      .then(res => getActivities(res))
+
+  }
+
+  reAuthorize();
+  
+
+  // },
+  //   []);
+
+
   // function 
 
   return (
--- client/src/utils/strava.js
+++ client/src/utils/strava.js
@@ -76,4 +76,4 @@
 
 // }
 
-// reAuthorize();
+// // reAuthorize();

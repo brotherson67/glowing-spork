f746a80197e28be41f7b69542cb52ebe16ecb221
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2022-02-15 23:14:51 -0700

    changed some of the running page

--- client/src/components/Run/RunBody.js
+++ client/src/components/Run/RunBody.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import "./run.css"
 
 function RunBody() {
   return (
     <div className="container">
-        <div>div 1 lorem ipsum dolor sit amet, consectetur lorem ipsum dolor sit amet</div>
-        <div>div 2 lorem ipsum dolor sit amet, consectetur lorem ipsum dolor sit amet</div>
+      <div className="runBody-box">
+        <div className="runBody-boxOuter">
+          <div className="runBody-boxInner">div 1 lorem ipsum dolor sit amet, consectetur lorem ipsum dolor sit amet</div>
+        </div>
+        <div className="runBody-boxOuter">
+          <div className="runBody-boxInner">div 1 lorem ipsum dolor sit amet, consectetur lorem ipsum dolor sit amet</div>
+        </div>
+        <div className="runBody-boxOuter">
+          <div className="runBody-boxInner">div 2 lorem ipsum dolor sit amet, consectetur lorem ipsum dolor sit amet</div>
+        </div>
+      </div>
+
 
     </div>
-    
+
   );
 }
 

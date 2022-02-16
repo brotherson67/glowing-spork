8c17d1d79d592d47572f0f6677135b43b24dca44
brotherson67 <mitchell.brotherson@gmail.com>
2022-02-16 01:16:29 -0700

    added psuedocode for search function

---
+++ client/src/utils/search.js
@@ -0,0 +1,10 @@
+// create function
+// create a variable that has a value from a column in mongoose
+// create an input variable
+// change input to lowercase
+
+// create a for loop
+// while i is less than the total amount of mongoose variables
+// if the contents of the variable at index i are not the same as the entered string
+// retrun false
+// else return the variable.

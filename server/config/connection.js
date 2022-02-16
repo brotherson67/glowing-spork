9b61b13ef1d5dea2206615ef4778fcabd6bab398
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2022-02-16 00:34:48 -0700

    heroku deploy test 3

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,26 @@
-const mongoose = require('mongoose');
+mongoose.connect(
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1/routes', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-});
 
-module.exports = mongoose.connection;
+  process.env.MONGODB_URI || 'mongodb://localhost/routes',
+
+
+  {
+
+
+    useNewUrlParser: true,
+
+
+    useUnifiedTopology: true,
+
+
+    useCreateIndex: true,
+
+
+    useFindAndModify: false,
+
+
+  },
+
+
+);
+

03886defed098c8dcad22e6e3e5251b6b7d81b5a
brotherson67 <mitchell.brotherson@gmail.com>
2022-02-16 01:08:05 -0700

    created searchbar object

---
+++ client/src/components/SeachBar/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Searchbar = () => {
+  return (
+    <div>
+      <input id="searchbar" placeholder="What are you looking for?" />
+    </div>
+  );
+};
+
+export default Searchbar;

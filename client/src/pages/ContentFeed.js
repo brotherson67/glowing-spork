df959f3fc39a8b804e92fcbc360f086288947f1a
brotherson67 <mitchell.brotherson@gmail.com>
2022-02-15 21:39:41 -0700

    server is starting

--- client/src/pages/ContentFeed.js
+++ client/src/pages/ContentFeed.js
@@ -1,34 +1,32 @@
-import React from 'react';
-import { useQuery } from '@apollo/client';
-import { QUERY_THOUGHTS } from '../utils/queries';
-import PostList from '../components/Posts';
-import StravaActivities from './Strava';
-
+import React from "react";
+import { useQuery } from "@apollo/client";
+import { QUERY_THOUGHTS } from "../utils/queries";
+import PostList from "../components/Posts";
+import StravaActivities from "./strava";
 
 const ContentFeed = () => {
-    //useQuery to make a query request
-    const { loading, data } = useQuery(QUERY_THOUGHTS);
-    //use optional chaining to negate checking if object exists before accessing its properties
-    const thoughts = data?.thoughts || [];
-    console.log(thoughts);
-// import coverImage from "../../assets/images/IMG_1939.JPEG";
+  //useQuery to make a query request
+  const { loading, data } = useQuery(QUERY_THOUGHTS);
+  //use optional chaining to negate checking if object exists before accessing its properties
+  const thoughts = data?.thoughts || [];
+  console.log(thoughts);
+  // import coverImage from "../../assets/images/IMG_1939.JPEG";
 
-    return (
+  return (
+    <div>
+      <section>
+        {/* <Hero img={coverImage}/> */}
         <div>
-            <section>
-            {/* <Hero img={coverImage}/> */}
-            <div>
-            {loading ? (
-        <div>Loading...</div>
-      ) : (
-        <PostList thoughts={thoughts} title="Find your next adventure!" />
-      )}
-            </div>
-            </section>
-            <StravaActivities />
-            
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <PostList thoughts={thoughts} title="Find your next adventure!" />
+          )}
         </div>
-    );
-}
+      </section>
+      <StravaActivities />
+    </div>
+  );
+};
 
 export default ContentFeed;
--- server/server.js
+++ server/server.js
@@ -1,22 +1,22 @@
-const express = require('express');
+const express = require("express");
 // import ApolloServer
-const { ApolloServer } = require('apollo-server-express');
-const { authMiddleware } = require('./utils/auth');
-const path = require('path');
+const { ApolloServer } = require("apollo-server-express");
+const { authMiddleware } = require("./utils/auth");
+const path = require("path");
 
 // import our typeDefs and resolvers
-const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 const startServer = async () => {
   // create a new Apollo server and pass in our schema data
-  const server = new ApolloServer({ 
-    typeDefs, 
-    resolvers, 
-    context: authMiddleware 
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
   });
 
   // Start the Apollo server
@@ -35,16 +35,16 @@ startServer();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-//serve up static assets 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+//serve up static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/"));
 });
 
-db.once('open', () => {
+db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
   });

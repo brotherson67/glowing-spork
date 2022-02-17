1b59490a869e4f0ec784e9371cc7166510f64068
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2022-02-16 19:34:42 -0700

    fixxed the chats to load preloaded data when clicked :

--- client/src/components/Chat/index.js
+++ client/src/components/Chat/index.js
@@ -5,16 +5,16 @@ import { Link } from 'react-router-dom';
 
 function Chat({ name, message, profilePic, timestamp }) {
   return (
-      //need to fix this to route to their name link to=`chat/chat/${name}`
-      <Link to={`/chatscreen`}>
-    <div className='chat'>
+    //need to fix this to route to their name link to=`chat/chat/${name}`
+    <Link to={`/chat/${name}`}>
+      <div className='chat'>
         <Avatar className='chat-img' alt={name} src={profilePic} />
         <div className='chat-details'>
-            <h2>{name}</h2>
-            <p>{message}</p>
+          <h2>{name}</h2>
+          <p>{message}</p>
         </div>
         <p className='chat-time'>{timestamp}</p>
-    </div>
+      </div>
     </Link>
   );
 }

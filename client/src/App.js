fd0b8d5c556ad59ec522f73dcd993e09cc880f4c
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2022-02-16 18:12:09 -0700

    Revert " i think i fixed the profile button/tab"
    This reverts commit 67db18cc5a68ac43f6b211dfc20ecccfd818c896.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,6 @@
-import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Home from './pages/Home';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
-
-import { setContext } from '@apollo/client/link/context';
 
 
 //eastablish link to graphql backend
@@ -11,18 +8,8 @@ const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
-
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: httpLink,
   cache: new InMemoryCache(),
 });
 
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,6 +1,7 @@
 
 import React from 'react';
 
+
 import { Navigate, useParams } from 'react-router-dom';
 
 import ThoughtForm from '../components/ThoughtForm';

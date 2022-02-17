648fda535def3fc885455f2acf596513a5870cf5
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2022-02-16 22:52:54 -0700

    strava and bike component

--- client/src/components/Bike/BikeBody.js
+++ client/src/components/Bike/BikeBody.js
@@ -4,14 +4,14 @@ import StravaActivities from '../../pages/strava';
 
 
 
-function BikeBody( ...props ) {
-  console.log(StravaActivities);
+function BikeBody( {name, distance, total_elevation_gain }) {
+  console.log(name);
   return (
     <div className="container">
         <div>
-          <h2>Activity name: {props.StavaActivities}</h2>
-          <h2>distance: {props.distance}</h2>
-          <h2>Elevation Gain:{props.total_elevation_gain}</h2>
+          <h2>Activity name: {name}</h2>
+          <h2>distance: {distance}</h2>
+          <h2>Elevation Gain:{total_elevation_gain}</h2>
         </div>
         <div>div 2 lorem ipsum dolor sit amet, consectetur lorem ipsum dolor sit amet</div>
 

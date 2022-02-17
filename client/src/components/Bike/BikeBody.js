227292588e1451e4982ed5dec1737816534a030e
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2022-02-17 14:21:52 -0700

    added map thing

--- client/src/components/Bike/BikeBody.js
+++ client/src/components/Bike/BikeBody.js
@@ -4,14 +4,15 @@ import StravaActivities from '../../pages/strava';
 
 
 
-function BikeBody( {name, distance, total_elevation_gain }) {
-  console.log(name);
+function BikeBody(props) {
+  console.log(props.data);
   return (
     <div className="container">
         <div>
-          <h2>Activity name: {name}</h2>
-          <h2>distance: {distance}</h2>
-          <h2>Elevation Gain:{total_elevation_gain}</h2>
+          <h1>{props.dataStrava}</h1>
+          <h2>Activity name: </h2>
+          {/* <h2>distance: {distance}</h2>
+          <h2>Elevation Gain:{total_elevation_gain}</h2> */}
         </div>
         <div>div 2 lorem ipsum dolor sit amet, consectetur lorem ipsum dolor sit amet</div>
 

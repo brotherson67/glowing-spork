7382de1f2d42097d42a796019ff61ed5a00ce889
brotherson67 <mitchell.brotherson@gmail.com>
2022-02-16 19:55:10 -0700

    server working

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ app.use(express.json());
 
 //serve up static assets
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });

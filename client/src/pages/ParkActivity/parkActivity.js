9956147f7e08e38708ba8fd2918650735b6b105f
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2022-02-18 20:42:57 -0700

    test

--- client/src/pages/ParkActivity/parkActivity.js
+++ client/src/pages/ParkActivity/parkActivity.js
@@ -15,106 +15,107 @@ var directionsUrl = [];
 var requestOptions = {
     method: 'GET',
     redirect: 'follow'
-  };
+};
 const apiKeyNps = "4fxi1Pok4kgde26ywgQnyEaaxknpMlmKw3svW5lP";
 
 function parkInfo() {
     // fetch a response from nps 
     fetch("https://developer.nps.gov/api/v1/parks?stateCode=" + inputSearchName.value + "&api_key=" + apiKeyNps + "&fields=description,fullName,url,activities,latitude,longitude,directionsInfo,addresses,weatherInfo,name,directionsUrl,operatingHours", requestOptions)
-    .then(response => response.json())
-    .then(result => { 
-    //    console.log(result.data[0]);
-        parks = result.data;
-        // console.log(parks)
-        var ulEl = document.createElement("ul");
-        // do all the html stuff to display the parks
-        for(var i = 0; i < parks.length; i++) {
-            
-            var liEl = document.createElement("li");
-            var aEl = document.createElement('a');
+        .then(response => response.json())
+        .then(result => {
+            //    console.log(result.data[0]);
+            parks = result.data;
+            // console.log(parks)
+            var ulEl = document.createElement("ul");
+            // do all the html stuff to display the parks
+            for (var i = 0; i < parks.length && parks.length > 5; i++) {
 
-            aEl.setAttribute('href',parks[i].url);
-            aEl.innerText = parks[i].fullName;
-            
-            liEl.appendChild(aEl);
-            ulEl.appendChild(liEl);
-        }
-        inputContainerEl.innerText = '';
-        inputContainerEl.appendChild( ulEl);
-    })
-    .catch(error => console.log('error', error));
+                var liEl = document.createElement("li");
+                var aEl = document.createElement('a');
+
+                aEl.setAttribute('href', parks[i].url);
+                aEl.innerText = parks[i].fullName;
+
+                liEl.appendChild(aEl);
+                ulEl.appendChild(liEl);
+            }
+                inputContainerEl.innerText = '';
+                inputContainerEl.appendChild(ulEl);
+
+        })
+        .catch(error => console.log('error', error));
 
 }
 
 function parkDirectionsInfo() {
     fetch("https://developer.nps.gov/api/v1/parks?stateCode=" + inputSearchName.value + "&api_key=" + apiKeyNps + "&fields=description,fullName,url,activities,latitude,longitude,directionsInfo,addresses,weatherInfo,name,directionsUrl,operatingHours", requestOptions)
-    .then(response => response.json())
-    .then(result => { 
-    //    console.log(result.data[0]);
-        directionsInfo = result.data;
-        var ulEl = document.createElement("ul");
-        // do all the html stuff to display the parks
-        for(var i = 0; i < directionsInfo.length; i++) {
-            
-            var liEl = document.createElement("li");
-            var aEl = document.createElement('a');
-            
-            aEl.setAttribute('href',directionsInfo[i].directionsUrl);
-            aEl.innerText = directionsInfo[i].directionsInfo;
-            liEl.appendChild(aEl);
-            ulEl.appendChild(liEl);
-        }
-        inputContainerEl.innerText = '';
-        inputContainerEl.appendChild( ulEl);
-    })
-    .catch(error => console.log('error', error));
+        .then(response => response.json())
+        .then(result => {
+            //    console.log(result.data[0]);
+            directionsInfo = result.data;
+            var ulEl = document.createElement("ul");
+            // do all the html stuff to display the parks
+            for (var i = 0; i < directionsInfo.length; i++) {
+
+                var liEl = document.createElement("li");
+                var aEl = document.createElement('a');
+
+                aEl.setAttribute('href', directionsInfo[i].directionsUrl);
+                aEl.innerText = directionsInfo[i].directionsInfo;
+                liEl.appendChild(aEl);
+                ulEl.appendChild(liEl);
+            }
+            inputContainerEl.innerText = '';
+            inputContainerEl.appendChild(ulEl);
+        })
+        .catch(error => console.log('error', error));
 
 
 }
 
 function parkWeatherInfo() {
     fetch("https://developer.nps.gov/api/v1/parks?stateCode=" + inputSearchName.value + "&api_key=" + apiKeyNps + "&fields=description,fullName,url,activities,latitude,longitude,directionsInfo,addresses,weatherInfo,name,directionsUrl,operatingHours", requestOptions)
-    .then(response => response.json())
-    .then(result => { 
-    //    console.log(result.data[0]);
-        weatherInfo = result.data;
-        var ulEl = document.createElement("ul");
-        // do all the html creation to display the parks weatherInfo
-        for(var i = 0; i < result.data.length; i++) {
-            
-            var liEl = document.createElement("li");
-            var aEl = document.createElement('a');
-            aEl.setAttribute('href',result.data[i].url);
-            aEl.innerText = result.data[i].weatherInfo;
-            liEl.appendChild(aEl);
-            ulEl.appendChild(liEl);
-        }
-        inputContainerEl.innerText = '';
-        inputContainerEl.appendChild( ulEl);
-    })
-    .catch(error => console.log('error', error));
+        .then(response => response.json())
+        .then(result => {
+            //    console.log(result.data[0]);
+            weatherInfo = result.data;
+            var ulEl = document.createElement("ul");
+            // do all the html creation to display the parks weatherInfo
+            for (var i = 0; i < result.data.length; i++) {
+
+                var liEl = document.createElement("li");
+                var aEl = document.createElement('a');
+                aEl.setAttribute('href', result.data[i].url);
+                aEl.innerText = result.data[i].weatherInfo;
+                liEl.appendChild(aEl);
+                ulEl.appendChild(liEl);
+            }
+            inputContainerEl.innerText = '';
+            inputContainerEl.appendChild(ulEl);
+        })
+        .catch(error => console.log('error', error));
 
 
 
 }
 
 
-var formData = function() {
-    inputSearchName.addEventListener('click', function(){
+var formData = function () {
+    inputSearchName.addEventListener('click', function () {
         console.log(inputSearchName);
     });
 };
 // var searchBtn = function() {
 //     // reacts to click on the search button
-searchCityBtn.addEventListener('click', function(){
-        // alert("search button was clicked");
-        parkInfo();
+searchCityBtn.addEventListener('click', function () {
+    // alert("search button was clicked");
+   
 });
 
-directionsInfoBtn.addEventListener('click', function(){
+directionsInfoBtn.addEventListener('click', function () {
     parkDirectionsInfo();
 })
-weatherInfoBtn.addEventListener('click', function(){
+weatherInfoBtn.addEventListener('click', function () {
     // console.log(parkWeatherInfo());
     parkWeatherInfo();
 })

8d39fb50c9632881fb6ea7ae9d3fd559c07a6d9e
brotherson67 <mitchell.brotherson@gmail.com>
2022-02-19 08:53:06 -0700

    added some styling to donation modal

--- client/src/components/DonationModal/index.js
+++ client/src/components/DonationModal/index.js
@@ -4,14 +4,18 @@ import "./Donations.css";
 
 export default function DonationModal() {
   const [modalIsOpen, setModalIsOpen] = useState(false);
+  const startDonation = () => {};
   return (
     <div>
       <button onClick={() => setModalIsOpen(true)}>donate</button>
       <Modal isOpen={modalIsOpen} id="donationModal">
         <h2>Thanks for Donating!!</h2>
-        <input type="text"></input>
-        <div>
-          <button onClick={() => setModalIsOpen(false)}> Close</button>
+        <input type="text" placeholder="Enter Donation Amount here"></input>
+        <div id="donationControls">
+          <button id="confirmAmount">Donate</button>
+          <button id="cancelDonation" onClick={() => setModalIsOpen(false)}>
+            Cancel
+          </button>
         </div>
       </Modal>
     </div>

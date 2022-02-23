a5c1bfbfefd3235115db0b9b48699475caf510b4
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2022-02-22 23:16:10 -0700

    pull from develop

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 // import ApolloServer
 const { ApolloServer } = require("apollo-server-express");
 const { authMiddleware } = require("./utils/auth");
-const { PubSub } = require("graphql-yoga")
+
 const path = require("path");
 
 // import our typeDefs and resolvers
@@ -11,13 +11,13 @@ const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const pubsub = new PubSub()
+
 const startServer = async () => {
   // create a new Apollo server and pass in our schema data
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: {authMiddleware, pubsub},
+    context: authMiddleware
   });
 
   // Start the Apollo server

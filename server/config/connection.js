7cc3011c77b4dd3b798502b745c9c19482a16e63
Mitch Brotherson <89677641+brotherson67@users.noreply.github.com>
2022-02-23 00:06:37 -0700

    Update connection.js
    changed connection to my db

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,8 @@
 
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/routes', {
+mongoose.connect(process.env.MONGODB_URI ||     "mongodb+srv://brotherson67:sIs6PwnP0IWmpopG@cluster0.pnqsj.mongodb.net/glowing-sporky?retryWrites=true&w=majority",
+ {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
